26dc89c273c4861b055d0840f786edcae68d477d
CKboss <qhn999@2008.sina.com>
2016-01-30 23:35:07 +0800

    problem 与 problemlist的对应

--- Handlers/ProblemHandler.py
+++ Handlers/ProblemHandler.py
@@ -7,55 +7,136 @@ from tools.dbcore import conn
 from tornado.concurrent import run_on_executor
 from concurrent.futures import ThreadPoolExecutor
 
-from Crawler.ProblemCrawler import HUSTProblemCralwer
-from Crawler.HduCrawler.HduCrawler import HduCrawler
-
+from tools.dbcore import conn
 from tools.encode import UTF8StrToBase64Str,Base64StrToUTF8Str
 
 class ProblemHandler(tornado.web.RequestHandler) :
 
-    executor = ThreadPoolExecutor(4)
+    executor = ThreadPoolExecutor(10)
 
     @tornado.web.asynchronous
     @tornado.gen.engine
     def get(self,oj=None,pid=None):
         print('oj: '+oj+' pid: '+pid)
-        yield self.getProblem(oj,pid)
-        self.render('problem.html')
+        D = yield self.getProblem(oj,pid)
+        self.render('problem.html',D = D)
 
 
     @run_on_executor
-    def getProblem(self,oj,pid):
+    def getProblem(self,oj,problemid):
 
-        pass
-        '''
+        # get pid
         cur = conn.cursor()
-        row = cur.fetchone()
 
-        if row is None :
-            if oj.upper() == 'HDU' or oj.upper() == 'HDOJ' :
-                crawler = HduCrawler()
-                crawler.CrawlerProblem(pid)
+        sql ='SELECT * FROM problem WHERE ( originOJ LIKE "{}" and originProb LIKE "{}" )'.format(oj,problemid)
+        cur.execute(sql)
+        rt = cur.fetchone()
 
-            cur.execute(sql)
-            row = cur.fetchone()
-            print('now find it! ')
+        #error
+        if rt is None : pass
 
-        Data = dict()
+        d = dict()
 
-        Data['title'] = row[1]
-        Data['description'] = Base64StrToUTF8Str(row[2])
-        Data['input'] = Base64StrToUTF8Str(row[3])
-        Data['output'] = Base64StrToUTF8Str(row[4])
-        Data['sample_in'] = Base64StrToUTF8Str(row[5])
-        Data['sample_out'] = Base64StrToUTF8Str(row[6])
-        Data['source'] = Base64StrToUTF8Str(row[25])
+        if rt[0] is not None :
+            d['pid'] = rt[0]
+        if rt[1] is not None :
+            d['title'] = rt[1]
+        if rt[2] is not None :
+            d['source'] = rt[2]
+        if rt[3] is not None :
+            d['url'] = rt[3]
 
-        self.render('problem.html',Data=Data)
-        '''
+        sql = 'SELECT * FROM problemdetail WHERE pid = {}'.format(d['pid'])
+        cur.execute(sql)
+        rt = cur.fetchone()
+
+
+        if rt[2] is not None :
+            d['description'] = Base64StrToUTF8Str(rt[2])
+        if rt[3] is not None :
+            d['input'] = Base64StrToUTF8Str(rt[3])
+        if rt[4] is not None :
+            d['output'] = Base64StrToUTF8Str(rt[4])
+        if rt[5] is not None :
+            d['sampleinput'] = Base64StrToUTF8Str(rt[5])
+        if rt[6] is not None :
+            d['sampleoutput'] = Base64StrToUTF8Str(rt[6])
+        if rt[7] is not None :
+            d['hint'] = Base64StrToUTF8Str(rt[7])
+        if rt[8] is not None :
+            d['author'] = Base64StrToUTF8Str(rt[8])
+        if rt[10] is not None :
+            d['updatetime'] = str(rt[10])
+        if rt[11] is not None :
+            d['memorylimit'] = rt[11]
+        if rt[12] is not None :
+            d['timelimit'] = rt[12]
+        if rt[13] is not None :
+            d['specialjudge'] = rt[13]
+
+        cur.close()
+        return d
+
 
 def main():
-    pass
+
+    oj = 'HDU'
+    problemid = '1092'
+
+    # get pid
+    cur = conn.cursor()
+
+    sql ='SELECT * FROM problem WHERE ( originOJ LIKE "{}" and originProb LIKE "{}" )'.format(oj,problemid)
+    cur.execute(sql)
+    rt = cur.fetchone()
+
+    #error
+    if rt is None : pass
+
+    d = dict()
+
+    if rt[0] is not None :
+        d['pid'] = rt[0]
+    if rt[1] is not None :
+        d['title'] = rt[1]
+    if rt[2] is not None :
+        d['source'] = rt[2]
+    if rt[3] is not None :
+        d['url'] = rt[3]
+
+    sql = 'SELECT * FROM problemdetail WHERE pid = {}'.format(d['pid'])
+    cur.execute(sql)
+    rt = cur.fetchone()
+
+
+    if rt[2] is not None :
+        d['description'] = Base64StrToUTF8Str(rt[2])
+    if rt[3] is not None :
+        d['input'] = Base64StrToUTF8Str(rt[3])
+    if rt[4] is not None :
+        d['output'] = Base64StrToUTF8Str(rt[4])
+    if rt[5] is not None :
+        d['sampleinput'] = Base64StrToUTF8Str(rt[5])
+    if rt[6] is not None :
+        d['sampleoutput'] = Base64StrToUTF8Str(rt[6])
+    if rt[7] is not None :
+        d['hint'] = Base64StrToUTF8Str(rt[7])
+    if rt[8] is not None :
+        d['author'] = Base64StrToUTF8Str(rt[8])
+    if rt[10] is not None :
+        d['updatetime'] = str(rt[10])
+    if rt[11] is not None :
+        d['memorylimit'] = rt[11]
+    if rt[12] is not None :
+        d['timelimit'] = rt[12]
+    if rt[13] is not None :
+        d['specialjudge'] = rt[13]
+
+    cur.close()
+
+    for key in d :
+        print(key+ ' ---> '+str(d[key]))
+
 
 if __name__=='__main__':
     main()
--- UIModule/ProblemListModule.py
+++ UIModule/ProblemListModule.py
@@ -7,4 +7,5 @@ class ProblemListModule(tornado.web.UIModule) :
         OJ = t[4]
         title = t[1]
         source = t[2]
-        return self.render_string('module/problemList.html',oj=OJ,id=problemID,title=title, source=source)
+        url = '/problem/'+OJ+'/'+problemID
+        return self.render_string('module/problemList.html',oj=OJ,id=problemID,title=title, source=source,url=url)
