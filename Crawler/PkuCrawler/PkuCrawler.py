cbb49a1482b6ca9236689632aaa63ee6ed99f2e0
CKboss <qhn999@2008.sina.com>
2016-02-07 17:33:18 +0800

    Pku 的 Crawler和Vjudger

---
+++ Crawler/PkuCrawler/PkuCrawler.py
@@ -0,0 +1,102 @@
+import requests
+import pickle
+import time
+from queue import Queue
+from bs4 import BeautifulSoup
+
+from tools.RandA import RelUrlToAbsUrl
+
+class PkuCrawler() :
+
+    base_url = 'http://poj.org/'
+    prob_url = 'http://poj.org/problem?id='
+
+    def CrawlerProblem(self,pid):
+
+        url = self.prob_url+str(pid)
+
+        data = dict()
+        data['originOJ'] = 'PKU'
+        data['originProb'] = pid
+        data['url'] = url
+
+        r = requests.get(url)
+        html = r.text
+        soup = BeautifulSoup(html,'html5lib').select_one('body > table:nth-of-type(2)')
+        html = str(soup)
+
+        d = self.getDetail(html)
+
+        for key in d :
+            data[key] = d[key]
+
+        Term = ['description','input','output','sampleinput','sampleoutput']
+
+        for t in Term :
+            data[t] = RelUrlToAbsUrl(self.base_url,data[t])
+
+        f = open('/home/ckboss/Desktop/Development/testData/POJ/POJ_%04d.pkl'%pid,'wb')
+
+        pickle.dump(data,f)
+        print(pid,' done!')
+
+    def getDetail(self,html):
+
+        #f = open('/tmp/f2.html','w')
+        #f.write(html)
+        f = open('/tmp/f2.html','r')
+        html = f.read()
+
+        d = dict()
+
+        soup = BeautifulSoup(html,'html5lib')
+        d['title'] = soup.select_one('body > table > tbody > tr > td > div.ptt').text
+
+        id = 4
+        for key in ['description','input','output','source'] :
+            d[key] = soup.select_one('body > table > tbody > tr > td > div:nth-of-type({})'.format(id))
+            id+=1
+        content = soup.find_all('pre',class_='sio')
+
+        d['sampleinput'] = content[0]
+        d['sampleoutput'] = content[1]
+
+        ''' get limit info'''
+
+        d['timelimit'] = soup.select_one('body > table > tbody > tr > td > div.plm > table > tbody > tr:nth-of-type(1) > td:nth-of-type(1)').text.replace('Time Limit:','')
+        d['memorylimit'] = soup.select_one('body > table > tbody > tr > td > div.plm > table > tbody > tr:nth-of-type(1) > td:nth-of-type(3)').text.replace('Memory Limit:','')
+        if soup.select_one('body > table > tbody > tr > td > div.plm > table > tbody > tr:nth-of-type(2) > td:nth-of-type(5)') is None :
+            d['specialjudge'] = 0
+        else :
+            d['specialjudge'] = 1
+
+        '''
+        for key in d :
+            print(key,' -->',d[key])
+            print('\n'*2)
+        '''
+
+        return d
+
+
+def crawlerFromTo(u,v) :
+
+    pc = PkuCrawler()
+    q = Queue()
+    for x in range(u,v+1) :
+        q.put(x)
+
+    while q.empty() == False :
+        pid = q.get()
+        time.sleep(0.5)
+        try :
+            pc.CrawlerProblem(pid)
+        except Exception :
+            q.put(pid)
+            print(pid,' error!!')
+
+def main():
+    crawlerFromTo(1000,4050)
+
+if __name__=='__main__' :
+    main()
---
+++ Crawler/PkuCrawler/PkuVJudger.py
@@ -0,0 +1,63 @@
+import requests
+import random
+import time
+
+from PkuConfig import Pku_User,Pku_LogIn_Url
+from tools.encode import UTF8StrToBase64Str,Base64StrToUTF8Str
+
+class PkuVJudger() :
+
+    '''
+    1.poj的提交需要对代码进行base64编码
+    2.每次都要登陆?
+    '''
+
+    headers = { 'User-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2364.103 Safari/537.36',}
+    submit_url = 'http://poj.org/submit?action=submit'
+    sl = 'http://poj.org/submit?action=submit'
+    s = None
+    snt = 0
+
+    def LogIn(self):
+        self.snt = 30
+        self.s = requests.session()
+        r = self.s.post(url=Pku_LogIn_Url,data=random.choice(Pku_User))
+
+    def GetLanguage(self,lang):
+
+        L = ['G++','GCC','Java','Pascal','C++','C','Fortran']
+        ret = 0
+        for i in range(0,7) :
+            if lang == L[i] :
+                ret = i
+                break
+        return ret
+
+    def Sumbit(self,pid,lang,code):
+
+        self.LogIn()
+        url = self.submit_url
+        code = UTF8StrToBase64Str(code)
+        d = dict()
+
+        d['problem_id'] = str(pid)
+        d['language'] = str(self.GetLanguage(lang))
+        d['source'] = code
+        d['encoded'] = str(1)
+
+        self.s.post(url=url,data=d)
+
+
+def main():
+    pv = PkuVJudger()
+    code ='#include <iostream> using namespace std; int a ,b cd#'
+    pv.Sumbit(2012,'Java',code)
+    pv.Sumbit(1001,'GCC',code)
+
+if __name__=='__main__' :
+    main()
+
+'''
+{'language': '4', 'source': 'CiNpbmNsdWRlIDxpb3N0cmVhbT4KdXNpbmcgbmFtZXNwYWNlIHN0ZDsKaW50IG1haW4oKQp7CiAgICBpbnQgYSxiOwogICAgY2luID4+IGEgPj4gYjsKICAgIGNvdXQgPDwgYStiIDw8IGVuZGw7CiAgICByZXR1cm4gMDsKfQogICAg', 'submit': 'Submit', 'problem_id': '1000', 'encode': '1'}
+I2luY2x1ZGUgPGlvc3RyZWFtPiB1c2luZyBuYW1lc3BhY2Ugc3RkOyBpbnQgYSAsYiBjZCM=
+'''
