1e05e8a24fa8cf0e8b3d7baeb0b70721326783c9
ckboss <qhn999@2008.sina.com>
2016-05-27 22:58:13 +0800

    Url转base64，对图片进行判断

--- tools/RandA.py
+++ tools/RandA.py
@@ -1,7 +1,6 @@
 import re
 from tools.PictureToBase64 import PictureToBase64
 
-
 def RelUrlToAbsUrl(baseurl, text):
     '''
     将/开头的相对url换成绝对url
@@ -11,9 +10,7 @@ def RelUrlToAbsUrl(baseurl, text):
     '''
 
     text = str(text)
-
     L = re.split(r' ', text)
-
     ret = ''
 
     for l in L:
@@ -39,14 +36,28 @@ def RelUrlToBase64Code(baseurl,text):
     L = re.split(r' ', text)
     ret = ''
 
+    picture_end = ['png','jpg','gif','bmp','svg','pcx']
+
     for l in L:
 
         if len(l) > 5 and l[:5] == 'src="':
             m = re.match(r'src="(.*)"',l)
             parturl=baseurl+'/'+m.group(1)
-            basedata = PictureToBase64(Url=parturl)
-            if basedata is not None :
-                l = l[:m.start(1)]+basedata+l[m.end(1):]
+
+            ispicture = False
+            url = m.group(1)
+            for end in picture_end :
+                if url.endswith(end) :
+                    ispicture = True
+                    break
+
+            if ispicture == True :
+                basedata = PictureToBase64(Url=parturl)
+                if basedata is not None :
+                    l = l[:m.start(1)]+basedata+l[m.end(1):]
+            elif ispicture == False :
+                if url[0] == '/':
+                    l = l[:m.start(1)]+baseurl+'/'+url+l[m.end(1):]
 
         ret = ret + ' ' + l
 
@@ -54,9 +65,10 @@ def RelUrlToBase64Code(baseurl,text):
 
 
 if __name__=='__main__':
-    f = open('/tmp/t1.html','r')
+    f = open('/tmp/t.html','r')
     baseurl = 'http://www.lydsy.com/JudgeOnline/'
+    #baseurl = 'http://acm.hdu.edu.cn/'
     ret = RelUrlToBase64Code(baseurl,f.read())
-    fout = open('/tmp/t3.html','w')
+    fout = open('/tmp/t2.html','w')
     fout.write(ret)
     fout.close()
